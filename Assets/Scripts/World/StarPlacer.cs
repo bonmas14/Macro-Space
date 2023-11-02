fd028f46b8a79a36d84f7aaa1533582aec4fd435
bonmas14 <bonmas14@gmail.com>
2023-11-02 23:59:48 +0300

    butler build and gitignore

--- Assets/Scripts/World/StarPlacer.cs
+++ Assets/Scripts/World/StarPlacer.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using TreeEditor;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class StarPlacer : MonoBehaviour
